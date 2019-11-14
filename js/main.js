a6c580eeca87ab1ca7ce57c132b5dade0cdd3468
John Brian <johnbryanjb011@gmail.com>
2019-11-14 08:02:58 +0300

    bug on registration button

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-
-
-
-function register(){
-    var name = document.getElementById("name").value;
-var street = document.getElementById("street").value;
-var age = document.getElementById("age").value;
-var vehicle = document.getElementById("vehicle").value
-    if (age <=17){
-        alert ("Hi son, Yo to young  for this try again in a few years")
-    }
-    else{
-        window.location.replace="race.html"
-    }
+function register() {
+  var name = document.getElementById("name").value;
+  var street = document.getElementById("street").value;
+  var age = document.getElementById("age").value;
+  var vehicle = document.getElementById("vehicle").value;
+  if (age <= 17) {
+    alert("Hi son, Yo to young  for this try again in a few years");
+  } else {
+    window.location.replace = "race.html";
+  }
 }
+
+
+
 
 
 // Set the date we're counting down to
@@ -20,13 +19,11 @@ var countDownDate = new Date("Nov 15, 2019 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
-
   // Get today's date and time
   var now = new Date().getTime();
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-  console.log("distance is"+distance)
 
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -35,8 +32,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
