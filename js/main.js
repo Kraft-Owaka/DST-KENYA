d9f6735bb7f27fd854af5878f83ed2ad71c7ef64
John Brian <johnbryanjb011@gmail.com>
2019-11-14 09:02:40 +0300

    fixed meta

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function register() {
   var name = document.getElementById("name").value;
   var street = document.getElementById("street").value;
-  var age = document.getElementById("age").value;
+  var age = parseInt(document.getElementById("age").value);
   var vehicle = document.getElementById("vehicle").value;
   if (age <= 17) {
     alert("Hi son, Yo to young  for this try again in a few years");
