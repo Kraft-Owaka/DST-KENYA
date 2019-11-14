5c1a1d46534d6b43b543f5b667c026c93c8c7bd6
trevor ngugi <ngugitrevor@gmail.com>
2019-11-14 08:20:06 +0300

    add toggle part but shows all races

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".sprints").click(function() {
+      $(".races").toggle();
+    });   
+  });
+
+  $(document).ready(function() {
+    $(".sprints").click(function() {
+      $(".races").toggle();
+    });   
+  });
+
+  $(document).ready(function() {
+    $(".sprints").click(function() {
+      $(".races").toggle();
+    });   
+  });
